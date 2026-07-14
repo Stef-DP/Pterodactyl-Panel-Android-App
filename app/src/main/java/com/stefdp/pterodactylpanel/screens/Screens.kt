9712e0f4bac60d21ade53254cb16960463712026
Stef-00012 <me@stefdp.com>
2026-07-14 23:43:42 +0200

    feat: add all screen definitions

--- app/src/main/java/com/stefdp/pterodactylpanel/screens/Screens.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/screens/Screens.kt
@@ -12,4 +12,43 @@ object LoadingScreen : AppScreen
 object LoginScreen : AppScreen
 
 @Serializable
-object HomeScreen : AppScreen
+object ClientServersScreen : AppScreen
+
+@Serializable
+object ClientAccountSettingsScreen : AppScreen
+
+@Serializable
+data class ClientServerScreen(val serverId: String) : AppScreen
+
+@Serializable
+object ApplicationLocationsScreen : AppScreen
+
+@Serializable
+data class ApplicationLocationScreen(val locationId: Long) : AppScreen
+
+@Serializable
+object ApplicationNodesScreen : AppScreen
+
+@Serializable
+data class ApplicationNodeScreen(val nodeId: Long) : AppScreen
+
+@Serializable
+object ApplicationServersScreen : AppScreen
+
+@Serializable
+data class ApplicationServerScreen(val serverId: Long) : AppScreen
+
+@Serializable
+object ApplicationUsersScreen : AppScreen
+
+@Serializable
+data class ApplicationUserScreen(val userId: Long) : AppScreen
+
+@Serializable
+object ApplicationNestsScreen : AppScreen
+
+@Serializable
+data class ApplicationNestScreen(val nestId: Long) : AppScreen
+
+@Serializable
+data class ApplicationNestEggScreen(val nestId: Long, val eggId: Long) : AppScreen
