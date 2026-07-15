7f148c107f5692a21fa04a5f5630cb839c7f5787
Stef-00012 <me@stefdp.com>
2026-07-15 19:55:58 +0200

    refactor: make Server stats parcelable

--- app/src/main/java/com/stefdp/pterodactylpanel/network/client/models/ServerStats.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/network/client/models/ServerStats.kt
@@ -1,22 +1,27 @@
 package com.stefdp.pterodactylpanel.network.client.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ServerStats(
     val `object`: String = "stats",
     val attributes: ServerStatsAttributes,
-)
+) : Parcelable
 
+@Parcelize
 data class ServerStatsAttributes(
     @SerializedName("current_state") val currentState: ServerState,
     @SerializedName("is_suspended") val isSuspended: Boolean,
     val resources: ServerStatsResources,
-)
+) : Parcelable
 
+@Parcelize
 data class ServerStatsResources(
     @SerializedName("memory_bytes") val memoryBytes: Long,
     @SerializedName("cpu_absolute") val cpuAbsolute: Double,
     @SerializedName("disk_bytes") val diskBytes: Long,
     @SerializedName("network_rx_bytes") val networkRxBytes: Long,
     @SerializedName("network_tx_bytes") val networkTxBytes: Long,
-)
+) : Parcelable
