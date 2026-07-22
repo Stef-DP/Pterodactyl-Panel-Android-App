dbd59bbc970325b8d6a8d5553c50ee4a0e380a86
Stef-00012 <me@stefdp.com>
2026-07-22 19:05:06 +0200

    chore: remove useless empty comments

--- app/src/main/java/com/stefdp/pterodactylpanel/ui/theme/Color.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/ui/theme/Color.kt
@@ -2,42 +2,42 @@ package com.stefdp.pterodactylpanel.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryDark = Color(0xFF2561E6) //
-val OnPrimaryDark = Color(0xFFEBF0FD) //
-val SecondaryDark = Color(0xFF626C7D) //
-val OnSecondaryDark = Color(0xFFF1F1F7) //
-val BackgroundDark = Color(0xFF33404C) //
-val OnBackgroundDark = Color(0xFFC7CDD7) //
-val SurfaceDark = Color(0xFF1F2933) //
-val OnSurfaceDark = Color(0xFFC3CAD3) //
-val SurfaceVariantDark = Color(0xFF16171D) //
-val OnSurfaceVariantDark = Color(0xFFF7F7F8) //
-val SurfaceContainerDark = Color(0xFF3F4A57) //
-val OutlineDark = Color(0xFF5F6B7B) //
+val PrimaryDark = Color(0xFF2561E6)
+val OnPrimaryDark = Color(0xFFEBF0FD)
+val SecondaryDark = Color(0xFF626C7D)
+val OnSecondaryDark = Color(0xFFF1F1F7)
+val BackgroundDark = Color(0xFF33404C)
+val OnBackgroundDark = Color(0xFFC7CDD7)
+val SurfaceDark = Color(0xFF1F2933)
+val OnSurfaceDark = Color(0xFFC3CAD3)
+val SurfaceVariantDark = Color(0xFF16171D)
+val OnSurfaceVariantDark = Color(0xFFF7F7F8)
+val SurfaceContainerDark = Color(0xFF3F4A57)
+val OutlineDark = Color(0xFF5F6B7B)
 val ErrorContainerDark = Color(0xFF991B1B)
-val ErrorDark = Color(0xFFD44246) //
-val OnErrorDark = Color(0xFFFAF8FB) //
-val LinkDark = Color(0xFF2B88F8) //
-val SurfaceDimDark = Color(0xFF282E39) //
+val ErrorDark = Color(0xFFD44246)
+val OnErrorDark = Color(0xFFFAF8FB)
+val LinkDark = Color(0xFF2B88F8)
+val SurfaceDimDark = Color(0xFF282E39)
 
 
-val PrimaryLight = Color(0xFF2561E6) //
-val OnPrimaryLight = Color(0xFFEBF0FD) //
-val SecondaryLight = Color(0xFF626C7D) //
-val OnSecondaryLight = Color(0xFFF1F1F7) //
-val BackgroundLight = Color(0xFF33404C) //
-val OnBackgroundLight = Color(0xFFC7CDD7) //
-val SurfaceLight = Color(0xFF1F2933) //
-val OnSurfaceLight = Color(0xFFC3CAD3) //
-val SurfaceVariantLight = Color(0xFF16171D) //
-val OnSurfaceVariantLight = Color(0xFFF7F7F8) //
-val SurfaceContainerLight = Color(0xFF3F4A57) //
-val OutlineLight = Color(0xFF5F6B7B) //
+val PrimaryLight = Color(0xFF2561E6)
+val OnPrimaryLight = Color(0xFFEBF0FD)
+val SecondaryLight = Color(0xFF626C7D)
+val OnSecondaryLight = Color(0xFFF1F1F7)
+val BackgroundLight = Color(0xFF33404C)
+val OnBackgroundLight = Color(0xFFC7CDD7)
+val SurfaceLight = Color(0xFF1F2933)
+val OnSurfaceLight = Color(0xFFC3CAD3)
+val SurfaceVariantLight = Color(0xFF16171D)
+val OnSurfaceVariantLight = Color(0xFFF7F7F8)
+val SurfaceContainerLight = Color(0xFF3F4A57)
+val OutlineLight = Color(0xFF5F6B7B)
 val ErrorContainerLight = Color(0xFF991B1B)
-val ErrorLight = Color(0xFFD44246) //
-val OnErrorLight = Color(0xFFFAF8FB) //
-val LinkLight = Color(0xFF2B88F8) //
-val SurfaceDimLight = Color(0xFF282E39) //
+val ErrorLight = Color(0xFFD44246)
+val OnErrorLight = Color(0xFFFAF8FB)
+val LinkLight = Color(0xFF2B88F8)
+val SurfaceDimLight = Color(0xFF282E39)
 
 
 val Yellow = Color(0xFFC38622)
