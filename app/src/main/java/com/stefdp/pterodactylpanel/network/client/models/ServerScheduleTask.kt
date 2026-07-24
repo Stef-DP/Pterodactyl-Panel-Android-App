a33cabf3119ed4445fd9d02df5ce1cc2b9272396
Stef-00012 <me@stefdp.com>
2026-07-24 02:52:42 +0200

    chore: changed task action from a string to an enum

--- app/src/main/java/com/stefdp/pterodactylpanel/network/client/models/ServerScheduleTask.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/network/client/models/ServerScheduleTask.kt
@@ -9,11 +9,25 @@ data class ServerScheduleTask(
     data class Attributes(
         val id: Long,
         @SerializedName("sequence_id") val sequenceId: Long,
-        val action: String,
+        val action: Action,
         val payload: String,
         @SerializedName("time_offset") val timeOffset: Long,
         @SerializedName("is_queued") val isQueued: Boolean,
+        @SerializedName("continue_on_failure") val continueOnFailure: Boolean,
         @SerializedName("created_at") val createdAt: String,
         @SerializedName("updated_at") val updatedAt: String? = null,
-    )
+    ) {
+        enum class Action(val value: String) {
+            @SerializedName("command")
+            COMMAND("command"),
+
+            @SerializedName("power")
+            POWER("power"),
+
+            @SerializedName("backup")
+            BACKUP("backup");
+
+            override fun toString(): String = value
+        }
+    }
 }
