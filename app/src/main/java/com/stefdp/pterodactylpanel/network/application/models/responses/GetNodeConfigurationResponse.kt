89c2c4be9fa53617848c4464607d22a3b481f5b1
Stef-00012 <me@stefdp.com>
2026-08-15 15:01:17 +0200

    chore: make 'GetNodeConfigurationResponse' serializable

--- app/src/main/java/com/stefdp/pterodactylpanel/network/application/models/responses/GetNodeConfigurationResponse.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/network/application/models/responses/GetNodeConfigurationResponse.kt
@@ -1,7 +1,9 @@
 package com.stefdp.pterodactylpanel.network.application.models.responses
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class GetNodeConfigurationResponse(
     val debug: Boolean,
     val uuid: String,
@@ -12,12 +14,14 @@ data class GetNodeConfigurationResponse(
     @SerializedName("allowed_mounts") val allowedMounts: List<String>,
     val remote: String
 ) {
+    @Serializable
     data class Api(
         val host: String,
         val port: Int,
         val ssl: Ssl,
         @SerializedName("upload_limit") val uploadLimit: Long
     ) {
+        @Serializable
         data class Ssl(
             val enabled: Boolean,
             val cert: String? = null,
@@ -25,10 +29,12 @@ data class GetNodeConfigurationResponse(
         )
     }
 
+    @Serializable
     data class System(
         val data: String,
         val sftp: Sftp,
     ) {
+        @Serializable
         data class Sftp(
             @SerializedName("bind_port") val bindPort: Int
         )
