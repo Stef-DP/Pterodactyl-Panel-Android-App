12bfa036f0a5bbf2927ccb32062c6ece723e407d
Stef-00012 <me@stefdp.com>
2026-08-17 22:45:24 +0200

    feat: add pagination to the list Nests HTTP request

--- app/src/main/java/com/stefdp/pterodactylpanel/network/application/models/responses/ListNestsResponse.kt
+++ app/src/main/java/com/stefdp/pterodactylpanel/network/application/models/responses/ListNestsResponse.kt
@@ -1,8 +1,28 @@
 package com.stefdp.pterodactylpanel.network.application.models.responses
 
+import com.google.gson.annotations.SerializedName
 import com.stefdp.pterodactylpanel.network.application.models.ApplicationNest
 
 data class ListNestsResponse(
     val `object`: String = "list",
-    val data: List<ApplicationNest>
-)
+    val data: List<ApplicationNest>,
+    val meta: Meta
+) {
+    data class Meta(
+        val pagination: Pagination
+    ) {
+        data class Pagination(
+            val total: Long,
+            val count: Long,
+            @SerializedName("per_page") val perPage: Long,
+            @SerializedName("current_page") val currentPage: Long,
+            @SerializedName("total_pages") val totalPages: Long,
+            val links: Links
+        ) {
+            data class Links(
+                val next: String? = null,
+                val previous: String? = null
+            )
+        }
+    }
+}
